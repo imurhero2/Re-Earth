8072cf8062d9c982a8e50931fcdc70d984290cca
Jean-Paul <35977754+imurhero2@users.noreply.github.com>
2020-02-01 12:18:45 -0500

    Player Movement Example
    Got a player controller to move around a globe. Now I would need to convert it into an AI that wanders wanders around the planet.

---
+++ Assets/Jean-Paul/Retired/FauxGravityBody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour
+{
+    public FauxGravityAttractor attractor;
+    public Rigidbody myRigidbody;
+    private Transform myTransform;
+
+    void Start()
+    {
+        myRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        myRigidbody.useGravity = false;
+        myTransform = transform;
+    }
+
+    void Update()
+    {
+        attractor.Attract(myTransform, myRigidbody);
+    }
+}
