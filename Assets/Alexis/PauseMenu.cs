cbd5fd49d2d6f7881c459cc8466102930cc96900
CroissantToes <CroissantToes@users.noreply.github.com>
2020-02-02 15:21:14 -0500

    derp

--- Assets/Alexis/PauseMenu.cs
+++ Assets/Alexis/PauseMenu.cs
@@ -43,7 +43,7 @@ public class PauseMenu : MonoBehaviour
     {
         Time.timeScale = 1f;
         GameIsPaused = false;
-        SceneManager.LoadScene("Main Menu");
+        SceneManager.LoadScene("Menu Backup 2");
     }
 
     public void QuitGame()
