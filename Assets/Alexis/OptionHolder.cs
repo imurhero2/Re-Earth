4dd574d1be77fdd28598f14ec5278ce041d64436
CroissantToes <CroissantToes@users.noreply.github.com>
2020-02-02 12:51:36 -0500

    Audio Sliders Work

---
+++ Assets/Alexis/OptionHolder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionHolder : MonoBehaviour
+{
+
+    public Slider music;
+    public Slider sfx;
+
+    void Start()
+    {
+
+        music.value = PlayerPrefs.GetFloat("mVol");
+        sfx.value = PlayerPrefs.GetFloat("sVol");
+    }
+
+    void Update()
+    {
+        PlayerPrefs.SetFloat("mVol", music.value);
+        PlayerPrefs.SetFloat("sVol", sfx.value);
+
+    }
+
+}
