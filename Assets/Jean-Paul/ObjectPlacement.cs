e15f32382ca30395311839caca39dd314a5852c0
CroissantToes <CroissantToes@users.noreply.github.com>
2020-02-01 19:04:04 -0500

    Audio and Essence

--- Assets/Jean-Paul/ObjectPlacement.cs
+++ Assets/Jean-Paul/ObjectPlacement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectPlacement : MonoBehaviour
 {
@@ -8,25 +9,32 @@ public class ObjectPlacement : MonoBehaviour
     public Transform planet;
     public LayerMask planetLayer;
     public LayerMask defaultLayer;
+   
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            // get raycast hit based on mouse position.
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast (ray, out hit, planetLayer))
+            if (Essence.essenceCount > 0)
             {
-                Debug.DrawLine(ray.origin, hit.point);
-                if (Physics.OverlapSphere(hit.point, 3, defaultLayer).Length < 1)
-                {
-                    // Calculate appropriate rotation based on planet.
-                    Quaternion toRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+                // get raycast hit based on mouse position.
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
 
-                    // Spawn object
-                    Instantiate(objectToSpawn, hit.point, toRotation);
+
+                if (Physics.Raycast(ray, out hit, planetLayer))
+                {
+                    Debug.DrawLine(ray.origin, hit.point);
+                    if (Physics.OverlapSphere(hit.point, 3, defaultLayer).Length < 1)
+                    {
+                        // Calculate appropriate rotation based on planet.
+                        Quaternion toRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+
+                        // Spawn object
+                        Instantiate(objectToSpawn, hit.point, toRotation);
+                        Essence.essenceCount -= 1;
+                       
+                    }
                 }
             }
         }
